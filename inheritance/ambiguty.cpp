c108ac0648e7a2b2711021e09785fc0d04b3c637
Ashish Singh <99067547+ashishsingh752@users.noreply.github.com>
2024-08-25 19:26:21 +0530

    Update ambiguty.cpp

--- inheritance/ambiguty.cpp
+++ inheritance/ambiguty.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
 using namespace std;
 
+/*
+ambiguity arises when a situation occurs where the compiler or runtime system cannot clearly determine which method, property, 
+or function to invoke due to multiple possible matches
+*/
+
 class A
 {
 public:
@@ -36,7 +41,7 @@ int main()
     C obj1;
     obj1.print1();
     // obj1.print()  // it is ambiguous for the C so we have to access the data as below
-    obj1.A::print();
+    obj1.A::print();  // To resolve the ambiguity, use the  scope resolution operator (::) to specify which base class's print() function to call
     obj1.B::print();
 
     return 0;
